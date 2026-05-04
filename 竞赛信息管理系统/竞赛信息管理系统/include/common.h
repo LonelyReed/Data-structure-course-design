c9f3de6deb489aacca4dbdd1f570a865c4fcdbc4
LonelyReed <cxlxh220416@163.com>
2026-05-04 16:07:44 +0800

    common.h全局设置

--- 竞赛信息管理系统/竞赛信息管理系统/include/common.h
+++ 竞赛信息管理系统/竞赛信息管理系统/include/common.h
@@ -1,1 +1,15 @@
 #pragma once
+
+//导入标准库头文件
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+//全局常量
+const std::string DATA_DIR = "data/";		//数据文件夹根目录
+const std::string STUDENT_FILE = "students.txt";
+const std::string CONSTEST_FILE = "contests.txt";
+const std::string AWARD_FILE = "awards.txt";
+const char DELTMITER = '|';					//分隔符
