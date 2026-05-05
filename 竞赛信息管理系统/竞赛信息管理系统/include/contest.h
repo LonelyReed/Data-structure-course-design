2b8ef8b37519fb8f78b73f441c06bae477154432
LonelyReed <cxlxh220416@163.com>
2026-05-05 14:05:10 +0800

    student.h和contest.h初步完成

--- 竞赛信息管理系统/竞赛信息管理系统/include/contest.h
+++ 竞赛信息管理系统/竞赛信息管理系统/include/contest.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "common.h"
+#include "linkedlist.h"
+
+class Contest
+{
+public:
+	std::string id;
+	std::string name;
+	std::string level;			//竞赛等级
+	std::string time;			//比赛时间
+
+	Contest() : id(""),name(""),level(""),time(""){}
+	Contest(std::string i, std::string n, std::string lev, std::string t)
+		: id(std::move(i)),name(std::move(n)),level(std::move(lev)),time(std::move(t)){ }
+
+	//运算重载
+	bool operator<(const Contest& c) const
+	{
+		return id < c.id;
+	}
+
+	bool operator==(const Contest& c) const
+	{
+		return id == c.id;
+	}
+};
+
+//<<流输出运算重载
+inline std::ostream& operator<<(std::ostream& os, const Contest& c)
+{
+	os << c.id << DELIMITER << c.name << DELIMITER << c.level << DELIMITER << c.time;
+	return os;
+}
--- 竞赛信息管理系统/竞赛信息管理系统/include/student.h
+++ 竞赛信息管理系统/竞赛信息管理系统/include/student.h
@@ -1,1 +1,41 @@
 #pragma once
+
+#include "common.h"
+#include "linkedlist.h"
+
+//定义学生节点的data(class和struct几乎没区别)
+class Student
+{
+public:
+	std::string id;
+	std::string name;
+	int grade;
+	std::string major;		//专业
+
+	//无参构造
+	Student() : id(""),name(""),grade(0),major(""){}
+	//带参构造
+	Student(std::string i,std::string n,int g,std::string m)
+		: id(std::move(i)),name(std::move(n)),grade(g),major(std::move(m)){ }
+
+	//<运算重载,按学号比较
+	bool operator<(const Student& s) const
+	{
+		return id < s.id;
+	}
+
+	//==运算重载
+	bool operator==(const Student& s) const
+	{
+		return id == s.id;
+	}
+
+	
+};
+
+//<<流输出重载
+inline std::ostream& operator<<(std::ostream& os, const Student& s)
+{
+	os << s.id << DELIMITER << s.name << DELIMITER << s.grade << DELIMITER << s.major;
+	return os;
+}
