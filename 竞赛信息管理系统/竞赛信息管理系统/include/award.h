33144b98185bf0568d6a0260c4da2b51cd717029
LonelyReed <cxlxh220416@163.com>
2026-05-06 22:52:45 +0800

    award.h中toLine和fromLine的添加

--- 竞赛信息管理系统/竞赛信息管理系统/include/award.h
+++ 竞赛信息管理系统/竞赛信息管理系统/include/award.h
@@ -26,6 +26,37 @@ public:
 	{
 		return awardID == a.awardID;
 	}
+
+	//将对象节点格式化为一行文本
+	std::string toLine() const
+	{
+		return std::to_string(awardID) + DELIMITER + studentID + DELIMITER + contestID + DELIMITER + prize + DELIMITER + time;
+	}
+
+	//从一行文本解析出对象节点——静态函数
+	static bool fromLine(const std::string& line, Award& out)					//定义为静态函数，是因为读取时候没有实例对象，只能依靠类直接调用生成实例对象
+	{
+		//空行直接放回false
+		if (line.empty())	return false;
+
+		// 按顺序读取：编号 -> '|' -> 学生学号 -> '|' -> 竞赛编号 -> '|' -> 获奖级别 -> '|' -> 获奖时间
+		std::stringstream ss(line);
+		int awardID;
+		std::string studentID, contestID, prize, time;
+		char delim;
+		if ((ss >> awardID >> delim)				//grade是int,先按照整数格式解析grade，解析完后就会停留在后面的'|'
+			&& delim == DELIMITER
+			&& std::getline(ss, studentID, DELIMITER)			//getline(ss,str,'|')只能读取str，遇到'|'就停止，并去除'|'
+			&& std::getline(ss, contestID, DELIMITER)
+			&& std::getline(ss, prize, DELIMITER)
+			&& std::getline(ss, time))
+		{
+			//全部读取成功，构造一个学生对象塞给 out
+			out = Award(awardID, studentID, contestID, prize, time);
+			return true;
+		}
+		return false;
+	}
 };
 
 //<<运算重载
