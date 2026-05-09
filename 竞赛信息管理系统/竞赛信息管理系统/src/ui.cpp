975da897571a6832d6b96ee606de9bed8cdb8284
LonelyReed <cxlxh220416@163.com>
2026-05-09 13:26:26 +0800

    ui中学生和竞赛子菜单完成

--- 竞赛信息管理系统/竞赛信息管理系统/src/ui.cpp
+++ 竞赛信息管理系统/竞赛信息管理系统/src/ui.cpp
@@ -67,7 +67,106 @@ void UI::studentMenu()
     int choice;
     do
     {
+        std::cout << "\n--- 学生管理 ---\n"
+            << "1. 添加学生\n"
+            << "2. 按学号删除学生及其获奖记录\n"
+            << "3. 按学号查找学生\n"
+            << "4. 列出所有学生\n"
+            << "0. 返回主菜单\n";
 
+        choice = getIntInput("请选择：");
+
+        switch (choice)
+        {
+        //添加学生
+        case 1:
+        {
+            std::string id = getStringInput("请输入学号：");
+            std::string name = getStringInput("请输入姓名：");
+            int grade = getIntInput("请输入年级：");
+            std::string major = getStringInput("请输入专业：");
+
+            //构建学生节点
+            Student s(id, name, grade, major);
+
+            //插入
+            if (db.insertStudent(s))
+            {
+                std::cout << "学生添加成功" << std::endl;
+            }
+            else
+            {
+                std::cout << "添加失败！学号可能已经存在" << std::endl;
+            }
+
+            pause();
+            break;
+        }
+        //按学号删除学生及其获奖记录
+        case 2:
+        {
+            std::string id = getStringInput("请输入要删除的学号：");
+            const Student* s = db.findStudent(id);
+
+            if (!s)
+            {
+                std::cout << "该学号不存在!" << std::endl;
+            }
+            else
+            {
+                std::string confirm = getStringInput("确定要删除学生" + s->id + " " + s->name + "及其所有获奖记录吗？[y/n]:");
+                if (confirm == "y" || confirm == "Y")
+                {
+                    db.removeStudentAwards(id);
+                    std::cout << "已删除学生及其获奖记录" << std::endl;
+                }
+                else
+                {
+                    std::cout << "已取消操作" << std::endl;
+                }
+            }
+
+            pause();
+            break;
+        }
+        //查找学生
+        case 3:
+        {
+            std::string id = getStringInput("请输入学号：");
+            const Student* s = db.findStudent(id);
+            
+            if (!s)
+            {
+                std::cout << "没有找到学生" << std::endl;
+            }
+            else
+            {
+                std::cout << s << std::endl;
+            }
+
+            pause();
+            break;
+        }
+        //打印所有学生信息
+        case 4:
+        {
+            db.printStudents();
+            pause();
+            break;
+        }
+        //退回主菜单
+        case 0:
+        {
+            break;
+        }
+        //无效输入
+        default:
+        {
+            std::cout << "无效输入选项，请重新输入" << std::endl;
+            pause();
+        }
+
+        }
     } while (choice != 0);
 }
 
@@ -77,6 +176,103 @@ void UI::contestsMenu()
     int choice;
     do
     {
+        std::cout << "\n---竞赛管理 ---\n"
+            << "1. 添加竞赛\n"
+            << "2. 按编号删除竞赛及其对应获奖记录\n"
+            << "3. 按编号查找竞赛\n"
+            << "4. 列出所有竞赛\n"
+            << "0. 返回主菜单\n";
+
+        choice = getIntInput("请选择：");
+
+        switch (choice)
+        {
+        //添加竞赛
+        case 1:
+        {
+            std::string id = getStringInput("请输入竞赛编号：");
+            std::string name = getStringInput("请输入竞赛名称：");
+            std::string level = getStringInput("请输入竞赛级别：");
+            std::string time = getStringInput("请输入举办日期(如 2026-05-01)：");
+
+            Contest c(id, name, level, time);
+
+            if (db.insertContest(c))
+            {
+                std::cout << "竞赛添加成功" << std::endl;
+            }
+            else
+            {
+                std::cout << "添加失败！竞赛可能已经存在" << std::endl;
+            }
+
+            pause();
+            break;
+        }
+        //按编号删除竞赛及其对应获奖记录
+        case 2:
+        {
+            std::string id = getStringInput("请输入要删除的竞赛编号：");
+            const Contest* c = db.findContest(id);
+
+            if (!c)
+            {
+                std::cout << "该竞赛不存在！" << std::endl;
+            }
+            else
+            {
+                std::string confirm = getStringInput("确定要删除竞赛" + c->id + " " + c->name + "及其对应获奖记录吗？[y/n]:");
+
+                if (confirm == "y" || confirm == "Y")
+                {
+                    db.removeContestAwards(id);
+                    std::cout << "竞赛信息及其获奖记录已删除" << std::endl;
+                }
+                else
+                {
+                    std::cout << "已取消操作" << std::endl;
+                }
+            }
+
+            pause();
+            break;
+        }
+        //按编号查找竞赛
+        case 3:
+        {
+            std::string id = getStringInput("请输入竞赛编号：");
+            const Contest* c = db.findContest(id);
+            if (!c)
+            {
+                std::cout << "没有找到该竞赛" << std::endl;
+            }
+            else
+            {
+                std::cout << c << std::endl;
+            }
+            pause();
+            break;
+        }
+        //打印所有竞赛
+        case 4:
+        {
+            db.printContests();
+            pause();
+            break;
+        }
+        //退回主菜单
+        case 0:
+        {
+            break;
+        }
+        //无效输入
+        default:
+        {
+            std::cout << "输入无效选项，请重新输入" << std::endl;
+            pause();
+        }
+
+        }
 
     } while (choice != 0);
 }
@@ -115,33 +311,40 @@ void UI::run()
         switch (choice)
         {
         case 1:
-            studentMenu(); 
+        {
+            studentMenu();
             break;
-        
+        }
         case 2:
+        {
             contestsMenu();
             break;
-
+        }
         case 3:
+        {
             awardMenu();
             break;
-
+        }
         case 4:
+        {
             queryMenu();
             break;
-
+        }
         case 5:
+        {
             db.clearAwards();
             std::cout << "获奖链表已清空" << std::endl;
             pause();        //等待下一步指令
             break;
-
+        }
         case 6:
+        {
             db.saveAll();
             pause();
             break;
-
+        }
         case 7:
+        {
             std::string confirm = getStringInput("确定要清空所有文件数据吗？此操作不可恢复！[y/n]:");
             if (confirm == "y" || confirm == "Y")        //不区分大小写
             {
@@ -155,15 +358,18 @@ void UI::run()
 
             pause();
             break;
-
+        }
         case 0:
+        {
             std::cout << "正在保存数据并退出..." << std::endl;
             db.saveAll();
             break;
-        
+        }
         default:
+        {
             std::cout << "无效输入选项，请重新输入" << std::endl;
             pause();
+        }
         }
     } while (choice != 0);
 }
